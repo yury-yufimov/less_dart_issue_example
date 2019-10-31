3ff56a9d79c4c20bd134963606e25685bf674fbb
yury.yufimov <yury.yufimov@team.wrike.com>
2019-10-31 13:46:23 +0300

    rename component

---
+++ main_pkg/lib/component_a.dart
@@ -0,0 +1,10 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'cmp-a',
+  styleUrls: ['component_a.css'],
+  templateUrl: 'component_a.html',
+)
+class ComponentA {
+  // Nothing here yet. All logic is in TodoListComponent.
+}
--- main_pkg/web/main.dart
+++ main_pkg/web/main.dart
@@ -1,6 +1,6 @@
 import 'package:angular/angular.dart';
-import 'package:main_pkg/app_component.template.dart' as ng;
+import 'package:main_pkg/component_a.template.dart' as ng;
 
 void main() {
-  runApp(ng.AppComponentNgFactory);
+  runApp(ng.ComponentANgFactory);
 }
