d8fac414418d47ef9c265a348f7d7cbbc36a7b15
yury.yufimov <yury.yufimov@team.wrike.com>
2019-11-19 20:17:45 +0300

    demo for components repo

---
+++ components_pkg/web/main.dart
@@ -0,0 +1,7 @@
+import 'package:angular/angular.dart';
+// ignore: uri_has_not_been_generated
+import 'package:components_pkg/button/button.template.dart' as ng;
+
+void main() {
+  runApp(ng.ButtonNgFactory);
+}
